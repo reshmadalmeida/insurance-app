aba906acd29ff4262d434f002c8d156c73b1617b
reshmadalmeida <60221759+reshmadalmeida@users.noreply.github.com>
2026-02-16 15:42:48 +0530

    Update LoginPage.jsx

--- frontend/src/features/auth/LoginPage.jsx
+++ frontend/src/features/auth/LoginPage.jsx
@@ -7,26 +7,37 @@ export default function LoginPage() {
   const { login } = useAuth();
   const [form, setForm] = useState({ username: '', password: '' });
   const [err, setErr] = useState(null);
+  const [isLoading, setIsLoading] = useState(false)
   const navigate = useNavigate();
   // const from = useLocation().state?.from?.pathname || '/dashboard';
-console.log(useLocation().state,"...loc")
+  console.log(useLocation().state, "...loc")
   const submit = async (e) => {
     e.preventDefault();
-    try { await login(form); 
+    setIsLoading(true);
+
+    try {
+      await login(form);
       navigate('./dashboard')
-      }
-      // navigate(from, { replace: true }); }
+    }
+    // navigate(from, { replace: true }); }
     catch { setErr('Invalid credentials'); }
+    finally {
+      setIsLoading(false);
+    }
   };
 
   return (
-    <Box sx={{ display:'grid', placeItems:'center', height:'100dvh' }}>
+    
+    <Box sx={{ display: 'grid', placeItems: 'center', height: '100dvh' }}>
+   
       <Paper sx={{ p: 4, width: 360 }} component="form" onSubmit={submit}>
+
         <Typography variant="h6" gutterBottom>Sign in to Insurance Portal</Typography>
-        <TextField label="Username" value={form.username} onChange={(e)=>setForm(f=>({ ...f, username: e.target.value }))} margin="dense" fullWidth />
-        <TextField label="Password" type="password" value={form.password} onChange={(e)=>setForm(f=>({ ...f, password: e.target.value }))} margin="dense" fullWidth />
+        <TextField label="Username" value={form.username} onChange={(e) => setForm(f => ({ ...f, username: e.target.value }))} margin="dense" fullWidth />
+        <TextField label="Password" type="password" value={form.password} onChange={(e) => setForm(f => ({ ...f, password: e.target.value }))} margin="dense" fullWidth />
         {err && <Typography color="error" variant="body2" sx={{ mt: 1 }}>{err}</Typography>}
-        <Button type="submit" fullWidth sx={{ mt: 2 }}>Login</Button>
+        <Button type="submit" disabled={!form.email || !form.password || isLoading}
+          fullWidth sx={{ mt: 2 }}>Login</Button>
       </Paper>
     </Box>
   );
